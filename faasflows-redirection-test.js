3716fdd05ab4b97b1bd922220fbddda02398dc12
Danendra Herdiansyah <danendra.herdiansyah@ui.ac.id>
2025-04-23 14:50:14 +0700

    Feat: Added first and second experiment test

---
+++ faasflows-redirection-test.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    stages: [
+        { duration: '20s', target: 10 },  // First stage: 10 users for 20 seconds
+        { duration: '40s', target: 50 },  // Second stage: 50 users for 40 seconds
+        { duration: '90s', target: 200 }, // Third stage: 200 users for 90 seconds
+    ],
+};
+
+export default function () {
+    const validUserIds = [10, 20, 30, 40];
+    const userId = validUserIds[Math.floor(Math.random() * validUserIds.length)];
+    const origin = `${(Math.random() * 90).toFixed(6)},${(Math.random() * 90).toFixed(6)}`;
+
+    const url = 'http://gateway.openfaas:8080/flow/homepage';
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    const payload = JSON.stringify({
+        user_id: userId,
+        origin: origin
+    });
+
+    http.post(url, payload, params);
+    sleep(1);
+}
